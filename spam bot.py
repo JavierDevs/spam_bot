c6fe976fb16885017dc977bd5b2dd8ce3f4cb509
JavierDevs <87899726+JavierDevs@users.noreply.github.com>
2021-07-29 19:14:41 +0800

    Update spam bot.py

--- spam bot.py
+++ spam bot.py
@@ -1,7 +1,11 @@
 import pyautogui as auto
 from time import sleep
+import keyboard
 
 while True:
-    auto.write('your_text')
+    auto.write('SPAM')
     auto.press('enter')
     sleep(0.1)
+    if keyboard.is_pressed('escape'):
+        while True:
+            sleep(1)
