1cb1f13d2410a93132849b63314108f5e4c9d6b4
JavierDevs <87899726+JavierDevs@users.noreply.github.com>
2021-07-31 16:42:57 +0800

    Update spam bot.py

--- spam bot.py
+++ spam bot.py
@@ -1,11 +1,34 @@
-import pyautogui as auto
+import pyautogui
+import appJar
 from time import sleep
 import keyboard
 
-while True:
-    auto.write('your_text')
-    auto.press('enter')
-    sleep(0.1)
-    if keyboard.is_pressed('escape'):
-        while True:
-            sleep(1)
+def buttonPress(button):
+    if(button == "Start"):
+        amount = int(app.getEntry("amount"))
+        text = str(app.getEntry("text"))
+        sleep(3)
+        for _ in range(amount):
+            pyautogui.write(text,interval=0)
+            pyautogui.press('enter')
+    if(button == "End"):
+        while keyboard.is_pressed('enter')==True:
+            pyautogui.sleep
+
+app = appJar.gui("Spam Bot")
+app.setSize("600x500")
+app.setSticky("new")
+app.addLabel("Spam Bot", row=0)
+app.setSticky("new")
+app.addLabel("Number of times spammed",row=1)
+app.addEntry("amount",row=2)
+app.setSticky("new")
+app.addLabel("Spammed text",row=3)
+app.addEntry("text", row=4)
+app.setSticky("new")
+app.addButton("Start", buttonPress, row=5)
+app.setSticky("new")
+app.addLabel("Click on where you want to spam after pressing start",row=6)
+app.setSticky("new")
+app.addButton("End", buttonPress, row=7)
+app.go()
